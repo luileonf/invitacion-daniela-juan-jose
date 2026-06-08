4b0a33cc8348a9b97c92c89e174692bb023df04e
luileonf <luileonf@gmail.com>
2026-06-08 12:40:45 -0600

    Connect wedding panel to Google Sheets endpoint

--- data-config.js
+++ data-config.js
@@ -1,2 +1,2 @@
-window.WEDDING_DATA_ENDPOINT = "";
+window.WEDDING_DATA_ENDPOINT = "https://script.google.com/macros/s/AKfycbwBEDSWZjjwrvbWAEiducRoTdMslG9RRyIMHgM3a1vuiOYT2lP_I5SbxuGx32nM-w_n/exec";
 window.WEDDING_SHEET_URL = "https://docs.google.com/spreadsheets/d/18eHNul9IiOm2Sibx8mgA77meRDqT-yP_37BwfPmrVv4";
